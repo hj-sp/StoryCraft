20c7d7c8960afed3bd179e8894b9e23053a84c47
HJJ <ailie03@naver.com>
2025-09-07 17:17:23 +0900

    chore: ignore node_modules

--- main.py
+++ main.py
@@ -34,10 +34,15 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "https://hj-sp.github.io",   
+    "http://localhost:5500",     
+    "http://127.0.0.1:5500",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins, 
     # allow_origins=["https://hj-sp.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
