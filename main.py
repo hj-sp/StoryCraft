30b82e48d94ff64acd73dabd4b0ad58705c3ffe3
HJJ <ailie03@naver.com>
2025-09-26 11:51:36 +0900

    js 수정

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ ALLOW_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
     "https://hj-sp.github.io",
     "https://crystal-0109.github.io",
 ]
